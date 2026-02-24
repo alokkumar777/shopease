dd04482f23e746cb7a2a14dc30fea817eeadb0bd
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2026-02-24 23:51:39 +0530

    update the app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Home />
-      <Login />
-      <Cart />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
